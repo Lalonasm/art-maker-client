1b669b979a0b780c311223e58bbe838dc432ff0f
Lalon Sheikh <you@example.com>
2024-07-15 06:50:10 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+       <h2 className="bg-red-500 font-extrabold">This is the Home page of Art Maker</h2>
+        </div>
+    );
+};
+
+export default Home;
