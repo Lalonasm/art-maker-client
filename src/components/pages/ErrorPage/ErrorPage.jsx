0a834ffd5cc2eca7d29399b8b382d55df3e8ff39
Lalon Sheikh <you@example.com>
2024-07-28 17:02:51 +0600

    404 page added

--- src/components/pages/ErrorPage/ErrorPage.jsx
+++ src/components/pages/ErrorPage/ErrorPage.jsx
@@ -20,9 +20,7 @@ const ErrorPage = () => {
                             >
                                 Go back home
                             </a></Link>
-                            {/* <a href="#" className="text-sm font-semibold text-gray-900">
-                                Contact support <span aria-hidden="true">&rarr;</span>
-                            </a> */}
+                           
                         </div>
                     </div>
                 </main>
